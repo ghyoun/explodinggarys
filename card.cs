5cfb2b719b4ab4f93408173a30fd522566cb1948
Mint <273greenst@gmail.com>
2016-09-15 11:33:02 -0700

    add action methods

--- card.cs
+++ card.cs
@@ -3,7 +3,6 @@ using System;
 namespace ConsoleApplication
 {
 
-    // -------------- General Card Class -----------------
     public class Card
     {
         public string name; 
@@ -11,26 +10,53 @@ namespace ConsoleApplication
         //"Attack","Defuse","Exploding_Kitten","Favor","Nope","See_the_Future","Shuffle","Skip"}
     
 
-        public void Card(string cardName)
+        public Card(string cardName)
         {
-            name = cardName
+            name = cardName;
+        }
+     
+        public object[] Attack()
+        {
+            // return false to required deal action and pass on 2 deals actoin to the next player               
+            object[] attack = new object[] {false,"deal_method_here","deal_method_here"};
+            return attack;
         }
 
-        // +++++++++++++ ACTION CARD METHOD ++++++++++++++++
-        public void Attack()
+        public bool Exploding_Kitten()
         {
-                
+            // return false to player.alive
+            return false;
         }
 
-        public void Exploding_Kitten()
+        public bool Skip()
         {
-            Player.alive = false;
+            // retur false to required deal action
+            return false;
         }
 
-        // +++++++++++++ END +++++++++++++++++++++++++++++++
+        public void Shuffle_card()
+        {
+            // call shuffle function
+            
+        }
+
+        public string[] See_Future()
+        {
+            // pick first top 3 cards on the top of the deck and return them back in order
+            string[] future = new string[3];
+            return future;
+        }
+
+        public void Nope()
+        {
+            // stop any action execpt an exploidng kitten or defuse card 
+            
+        }
+
+
 
     }
 
-        // -------------- END --------------------------------
+      
 
 }
