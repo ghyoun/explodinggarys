59293c7900a780b8a2454ade1ad32c1d114b4c27
ghyoun <ghyoun@uw.edu>
2016-09-15 14:12:57 -0700

    finished discard and three of a kind

--- discard.cs
+++ discard.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace ConsoleApplication
 {
@@ -13,9 +14,50 @@ namespace ConsoleApplication
             pile = new Card[0];
         }
 
+        //adds a card to discard pile
+        //takes in card to be discarded
+        public void addToDiscard(Card trash)
+        {
+            Array.Resize(ref pile, pile.Length+1);
+            pile[pile.Length-1] = trash;
+
+        }
+
+        //returns number of cards in the discard pile
         public int cardsInDiscard()
         {
             return pile.Length;
+        }
+
+        //returns a set of unique cards in the discard pile
+        public HashSet<String> uniqueCards()
+        {
+            HashSet<String> uniques = new HashSet<String>();
+            foreach(Card card in pile)
+            {
+                uniques.Add(card.name);
+            }
+            return uniques;
+        }
+
+        //takes in a card wanted from the discard pile
+        //returns the card we want
+        public Card takeFromDiscard(string wantedCard)
+        {
+            Card holder;
+            for (int i = 0; i < pile.Length; i++) {
+                if (pile[i].name == wantedCard)
+                {
+                    holder = pile[i];
+                    for (int j = i; j < pile.Length; j++)
+                    {
+                        pile[i] = pile[i+1];
+                    }
+                    Array.Resize(ref pile, pile.Length-1);
+                    return holder;
+                }
+            }
+            return null;
         }
     }
 
